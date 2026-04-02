46cc6f0e4cdfbf34c04022a3996a5bc4a6a046c6
Raina81 <rainachakraborty14@gmail.com>
2026-04-02 11:13:54 +0530

    Create Swapping Numbers using 2 variables.c

---
+++ Swapping Numbers using 2 variables.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main(){
+int a,b;
+printf("Enter the first number");
+scanf("%d",&a);
+printf("Enter the second number");
+scanf("%d",&b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("First number: %d",a);
+printf("\n");
+printf("second number: %d",b);
+}
